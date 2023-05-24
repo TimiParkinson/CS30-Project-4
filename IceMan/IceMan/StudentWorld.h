bd8ac8c80059235af021f3f8c4fe4366c72b4544
theOriginalMe <timi.parkinson@gmail.com>
2023-05-23 23:27:19 -0700

    Added Features: StudentWorld::init(), StudentWorld::move(), StudentWorld::cleanUp(), and appropriate data members.

--- IceMan/IceMan/StudentWorld.h
+++ IceMan/IceMan/StudentWorld.h
@@ -3,51 +3,65 @@
 
 #include "GameWorld.h"
 #include "GameConstants.h"
-#include "Actor.h"
+#include <algorithm>
+#include <array>
 #include <unordered_set>
 #include <string>
+
+class Actor;
+class Iceman;
+class Ice;
 
 // Students:  Add code to this file, StudentWorld.cpp, Actor.h, and Actor.cpp
 
 class StudentWorld : public GameWorld {
  private:
-	struct GameStats {
+	class GameStats {
+	 private:
 		int m_level;
 		int m_lives;
+		int m_score;
 		int m_health;
 		int m_squirts;
 		int m_gold;
-		int m_barrelsLeft;
+		int m_barrels;
 		int m_sonar;
-		int score;
+
+		int m_boulders;
+	 public:
+		GameStats(/*StudentWorld* stw */) /*: m_level(stw->getLevel()), m_lives(stw->getLives()), m_score(stw->getScore())*/ {}
+		void init() noexcept {
+			m_boulders = min(m_level / 2 + 2, 9);
+			m_gold = max(5 - m_level / 2, 2);
+			m_barrels = min(2 + m_level, 21);
+		}
+		std::string toString() {
+			return "<game statistics>";
+		}
 	};
 	GameStats m_stats;
-	StudentWorld* IceMan;
-	//integer value is arbitrary, just need a key to access the value
+	Iceman* m_iceMan;
 	std::unordered_set<Actor*> m_actors;
+	std::array<std::array<Ice*, 60>, 60> m_oilField;
+
  public:
-	StudentWorld(std::string assetDir) : GameWorld(assetDir) {
-
-	}
-
-	virtual int init() {
-
-		return GWSTATUS_CONTINUE_GAME;
-	}
-
-	virtual int move() {
-		// This code is here merely to allow the game to build, run, and terminate after you hit enter a few times.
-		// Notice that the return value GWSTATUS_PLAYER_DIED will cause our framework to end the current level.
-		for (const auto& it : m_actors) {
-			it.move();
+	StudentWorld(std::string assetDir) : GameWorld(assetDir), m_iceMan(nullptr) {
+		for (auto i : m_actors) {
+			i = nullptr;
 		}
-		decLives();
-		return GWSTATUS_PLAYER_DIED;
+		for (auto& i : m_oilField) {
+			for (auto& j : i)
+			j = nullptr;
+		}
 	}
 
-	virtual void cleanUp() {
-	}
+	virtual int init();
 
+	virtual int move();
+
+	virtual void cleanUp();
+
+	~StudentWorld() {}
 };
 
 #endif // STUDENTWORLD_H_

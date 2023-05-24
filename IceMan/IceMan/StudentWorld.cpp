6535cd930d0c9d73d7b34145d047005aed1d85bd
theOriginalMe <timi.parkinson@gmail.com>
2023-05-24 11:11:41 -0700

    Added Features: Abstraction of Oil Field and Actor managing data members into respective classes.

--- IceMan/IceMan/StudentWorld.cpp
+++ IceMan/IceMan/StudentWorld.cpp
@@ -7,87 +7,69 @@ GameWorld* createStudentWorld(string assetDir) {
 	return new StudentWorld(assetDir);
 }
 
+//StudentWolrd Implementation
+
 int StudentWorld::init() {
-	//initialize data structures (std::unordered_set<>)
-	for (auto i : m_actors) {
-		i = nullptr;
-	}
+	//initialize stats
+	m_stats.init();
+
+	//initialize stage for actors
+	m_stage.init();
+
 	//construct oil field
-	for (int i = 0; i <= 60 * 60; i++) {
-		int x = i / 61;
-		int y = i % 61;
+	m_oilField.init();
 
-		if ((x >= 30 && x <= 33) && (y >= 4 && y <= 59))
-			m_oilField[x][y] = nullptr;
-		else
-			m_oilField[x][y] = new Ice(x, y);
-	}
-	/*for  (int x = 0; x < 60; x++) {
-		for (int y = 0; y < 60; y++) {
-			if ((x >= 30 && x <= 33) && (y >= 4 && y < 59))
-				m_oilField[x][y] = nullptr;
-			else
-				m_oilField[x][y] = new Ice(x, y);
-		}
-	}*/
-
-	/*for (auto& xAxis : m_oilField) {
-		for (auto& yValue : xAxis) {
-			if ((x >= 30 and x <= 33) and (y >= 4 and y <= 59))
-				yValue == nullptr;
-			else
-				yValue = new Ice(x, y);
-			y++;
-		}
-		x++;
-	}*/
 	//allocate and insert iceman
-	m_iceMan = new Iceman(this);
+	m_iceman = new Iceman(this);
 
 	return GWSTATUS_CONTINUE_GAME;
 }
 
 int StudentWorld::move() {
+	// Display Stats
 	setGameStatText(m_stats.toString());
 
-	for (const auto& i : m_actors) {
-	    if (i != nullptr)
-    		i->doSomething();
-	}
-	m_iceMan->doSomething();
+	// Give each actor a chance to do something
+	m_stage.move();
+
+	//Give player a chance to do something
+	m_iceman->doSomething();
 
 	// Notice that the return value GWSTATUS_PLAYER_DIED will cause our framework to end the current level.
 	return GWSTATUS_CONTINUE_GAME;
 }
 
 void StudentWorld::cleanUp() noexcept {
-    delete m_iceMan;
-	m_iceMan = nullptr;
-    for (auto& i : m_oilField) {
-		for (auto& j : i) {
-			delete j;
-			j = nullptr;
-		}
-	}
-	for (auto i : m_actors) {
-		delete i;
-		i = nullptr;
-	}
+    delete m_iceman;
+	m_iceman = nullptr;
+    m_oilField.cleanUp();
+	m_stage.cleanUp();
 }
 
 void StudentWorld::removeIce() noexcept {
-	int endX = std::min(m_iceMan->getX() + 3, 59);
-	int endY = std::min(m_iceMan->getY() + 3, 59);
+	int endX = std::min(m_iceman->getX() + 3, 59);
+	int endY = std::min(m_iceman->getY() + 3, 59);
 
-	for (int i = m_iceMan->getX(); i <= endX; i++) {
-		for (int j = m_iceMan->getY(); j <= endY; j++) {
-			cout << "iceMan x: " << m_iceMan->getX() << " iceMan y: " << m_iceMan->getY() << endl;
+	for (int i = m_iceman->getX(); i <= endX; i++) {
+		for (int j = m_iceman->getY(); j <= endY; j++) {
+			cout << "iceMan x: " << m_iceman->getX() << " iceMan y: " << m_iceman->getY() << endl;
 			cout << "i: " << i << " j: " << j << endl << endl;
-			if (m_oilField[i][j] != nullptr) {
-				m_oilField[i][j]->setVisible(false);
-				delete m_oilField[i][j];
-				m_oilField[i][j] = nullptr;
-			}
+			m_oilField.removeIce(i, j);
 		}
 	}
 }
+
+//OilField Implementation
+
+void StudentWorld::OilField::init() {
+	for (int x = 0; x < 60; x++) {
+		for (int y = 4; y < 60; y++){
+			if (x >= 30 && x <= 33 && y >= 4 && y <= 59) {
+				continue;
+			}
+			self[x][y] = new Ice(x, y);
+		}
+	}
+}
+
+//Stage Implementation
--- IceMan/IceMan/StudentWorld.h
+++ IceMan/IceMan/StudentWorld.h
@@ -29,42 +29,102 @@ class StudentWorld : public GameWorld {
 
 		int m_boulders;
 	 public:
-		GameStats(/*StudentWorld* stw */) /*: m_level(stw->getLevel()), m_lives(stw->getLives()), m_score(stw->getScore())*/ {}
+		GameStats(StudentWorld* stw ) : m_level(stw->getLevel()), m_lives(stw->getLives()), m_score(stw->getScore()) {}
 		void init() noexcept {
 			m_boulders = std::min(m_level / 2 + 2, 9);
 			m_gold = std::max(5 - m_level / 2, 2);
 			m_barrels = std::min(2 + m_level, 21);
 		}
-		std::string toString() {
+		std::string toString() const noexcept {
 			return "<game statistics>";
 		}
 	};
+	class OilField {
+	 private:
+	 	std::array<std::array<Ice*, 60>, 60> self;
+	 public:
+		OilField() {
+			for (auto& i : self) {
+				for (auto& j : i)
+					j = nullptr;
+			}
+		}
+		~OilField() {
+			for (auto& i : self) {
+				for (auto& j : i)
+					delete j;
+			}
+		}
+
+		void cleanUp() noexcept {
+			for (auto& i : self) {
+				for (auto& j : i) {
+					delete j;
+					j = nullptr;
+				}
+			}
+		}
+		void removeIce(int x, int y) noexcept {
+			if (self[x][y] != nullptr) {
+				self[x][y]->setVisible(false);
+				delete self[x][y];
+				self[x][y] = nullptr;
+			}
+		}
+		void init();
+	};
+	class Stage {
+	 private:
+	 	std::unordered_set<Actor*> self;
+
+	 public:
+		Stage() {
+			for (auto i : self) {
+				i = nullptr;
+			}
+		}
+		~Stage() {
+			for (auto i : self) {
+				delete i;
+				i = nullptr;
+			}
+		}
+
+		void cleanUp() noexcept {
+			for (auto i : self) {
+				delete i;
+				i = nullptr;
+			}
+		}
+		void addActor(Actor* actor) noexcept {
+			self.insert(actor);
+		}
+		void removeActor(Actor* actor) noexcept {
+			self.erase(actor);
+		}
+		void init() {}
+		void move() {}
+	};
+
 	GameStats m_stats;
-	Iceman* m_iceMan;
-	std::unordered_set<Actor*> m_actors;
-	std::array<std::array<Ice*, 60>, 60> m_oilField;
+	Iceman* m_iceman;
+	Stage m_stage;
+	OilField m_oilField;
 
  public:
-	StudentWorld(std::string assetDir) : GameWorld(assetDir), m_iceMan(nullptr) {
-		for (auto i : m_actors) {
-			i = nullptr;
-		}
-		for (auto& i : m_oilField) {
-			for (auto& j : i)
-			j = nullptr;
-		}
-	}
+	StudentWorld(std::string assetDir) : GameWorld(assetDir), m_iceman(nullptr), m_stats(this) {}
 
-	virtual int init();
+	virtual int init() override;
 
-	virtual int move();
+	virtual int move() override;
 
-	virtual void cleanUp() noexcept;
+	virtual void cleanUp() noexcept override ;
 
 	void removeIce() noexcept;
 
 	~StudentWorld() {
-		cleanUp();
+		delete m_iceman;
+		m_iceman = nullptr;
 	}
 };
 

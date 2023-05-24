140268fb930ca3000ebe648a0b888be1ed90d033
theOriginalMe <timi.parkinson@gmail.com>
2023-05-23 23:27:19 -0700

    Added Features: StudentWorld::init(), StudentWorld::move(), StudentWorld::cleanUp(), and appropriate data members.

--- IceMan/IceMan/Actor.h
+++ IceMan/IceMan/Actor.h
@@ -3,6 +3,58 @@
 
 #include "GraphObject.h"
 
-// Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
+class StudentWorld;
+
+class Actor : public GraphObject {
+public:
+	Actor(int imageID, int startX, int startY, Direction dir = right, double size = 1.0, unsigned int depth = 0) : GraphObject(imageID, startX, startY, dir, size, depth) {}
+	~Actor() {}
+
+	virtual void onTick() = 0;
+};
+
+#pragma region Entities
+class Entity : public Actor {
+public:
+	Entity(int imageID, int startX, int startY) : Actor(imageID, startX, startY) {}
+	~Entity() {}
+};
+
+class Iceman : public Entity {
+private:
+	StudentWorld* m_studentWorldPointer;
+public:
+	Iceman(StudentWorld* sp = nullptr): Entity(IID_PLAYER, 30, 60), m_studentWorldPointer(sp) { setVisible(true); }
+	~Iceman() {}
+
+	StudentWorld* getWorld() const;
+	void onTick();
+};
+#pragma endregion Entities
+
+#pragma region GameObjects
+class Object : public Actor {
+public:
+	Object(int imageID, int startX = 0, int startY = 0, Direction dir = right, double size = 1.0, unsigned int depth = 0) : Actor(imageID, startX, startY, dir, size, depth) {}
+	~Object(){}
+};
+
+class Terrain : public Object {
+public:
+	Terrain(int imageID, int startX, int startY, Direction dir = right, double size = 1.0, unsigned int depth = 0) : Object(imageID, startX, startY, dir, size, depth) {}
+	Terrain(int imageID) : Object(imageID) {}
+	~Terrain() {}
+};
+
+class Ice : public Terrain {
+public:
+	Ice(int startX, int startY, Direction dir = right, double size = 1.0, unsigned int depth = 3) : Terrain(IID_ICE, startX, startY, dir, size, depth) {
+		setVisible(true);
+		//std::cout << "created ICE" << std::endl;
+	}
+	~Ice() {}
+	void onTick() {}
+};
+#pragma endregion GameObjects
 
 #endif // ACTOR_H_
--- IceMan/IceMan/StudentWorld.cpp
+++ IceMan/IceMan/StudentWorld.cpp
@@ -1,10 +1,61 @@
 #include "StudentWorld.h"
+#include "Actor.h"
 #include <string>
 using namespace std;
 
-GameWorld* createStudentWorld(string assetDir)
-{
+GameWorld* createStudentWorld(string assetDir) {
 	return new StudentWorld(assetDir);
 }
 
-// Students:  Add code to this file (if you wish), StudentWorld.h, Actor.h and Actor.cpp
+int StudentWorld::init() {
+	//initialize data structures (std::unordered_set<>)
+	for (auto i : m_actors) {
+		i = nullptr;
+	}
+	//construct oil field
+	int x;
+	int y;
+	for (auto& xAxis : m_oilField) {
+		for (auto& yValue : xAxis) {
+			if ((x >= 30 and x <= 33) and (y >= 4 and y <= 59))
+				yValue == nullptr;
+			else
+				yValue = new Ice(x, y);
+			y++;
+		}
+		x++;
+	}
+	//allocate and insert iceman
+	m_iceMan = new Iceman(this);
+
+	return GWSTATUS_CONTINUE_GAME;
+}
+
+int StudentWorld::move() {
+	setGameStatText(m_stats.toString());
+	
+	for (const auto& i : m_actors) {
+	    if (i != nullptr)
+    		i->onTick();
+	}
+
+	m_iceMan->onTick();
+
+	// Notice that the return value GWSTATUS_PLAYER_DIED will cause our framework to end the current level.
+	return GWSTATUS_PLAYER_DIED;
+}
+
+void StudentWorld::cleanUp() noexcept {
+    delete m_iceMan;
+	m_iceMan = nullptr;
+    for (auto& i : m_oilField) {
+		for (auto& j : i) {
+			delete j;
+			j = nullptr;
+		}
+	}
+	for (auto i : m_actors) {
+		delete i;
+		i = nullptr;
+	}
+}

bd1eb864f84734ba0077628c9f62cda611e4e795
theOriginalMe <timi.parkinson@gmail.com>
2023-05-23 23:27:19 -0700

    Added Features: StudentWorld::init(), StudentWorld::move(), StudentWorld::cleanUp(), and appropriate data members.

--- IceMan/IceMan/Actor.cpp
+++ IceMan/IceMan/Actor.cpp
@@ -1,4 +1,23 @@
 #include "Actor.h"
 #include "StudentWorld.h"
 
-// Students:  Add code to this file (if you wish), Actor.h, StudentWorld.h, and StudentWorld.cpp
+StudentWorld* Iceman::getWorld() const  {
+	return m_studentWorldPointer;
+}
+
+void Iceman::onTick() {
+	int ch;
+	if (getWorld()->getKey(ch)) {
+		switch (ch) {
+		case KEY_PRESS_LEFT:
+			moveTo(getX() - 1, getY());
+			break;
+		case KEY_PRESS_RIGHT:
+			moveTo(getX() + 1, getY());
+			break;
+		//case KEY_PRESS_SPACE:
+		//	// add a squirt in front of the player
+		//	break;
+		}
+	}
+}

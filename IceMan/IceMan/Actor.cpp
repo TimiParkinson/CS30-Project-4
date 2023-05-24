5dd6cfe722d8c0c71bcb26f134cf69455a5318a2
theOriginalMe <timi.parkinson@gmail.com>
2023-05-24 12:44:27 -0700

    Added Features: Cleaned Up Abstraction. Updated Actor class to change directions.

--- IceMan/IceMan/Actor.cpp
+++ IceMan/IceMan/Actor.cpp
@@ -1,25 +1,29 @@
 #include "Actor.h"
 #include "StudentWorld.h"
 
-StudentWorld* Iceman::getWorld() const  {
+StudentWorld* Iceman::getWorld() const noexcept  {
 	return m_studentWorldPointer;
 }
 
-void Iceman::onTick() {
+void Iceman::doSomething() {
 	int ch;
 	if (getWorld()->getKey(ch)) {
 		switch (ch) {
 			case KEY_PRESS_LEFT:
+                setDirection(Direction::left);
 				moveTo(getX() - 1, getY());
 				break;
 			case KEY_PRESS_RIGHT:
+                setDirection(Direction::right);
 				moveTo(getX() + 1, getY());
 				break;
 			case KEY_PRESS_UP:
+                setDirection(Direction::up);
 				moveTo(getX(), getY() + 1);
 				break;
 			case KEY_PRESS_DOWN:
-				moveTo(getX(), getY() - 1);
+                setDirection(Direction::down);
+                moveTo(getX(), getY() - 1);
 				break;
 			//case KEY_PRESS_SPACE:
 			//	// add a squirt in front of the player

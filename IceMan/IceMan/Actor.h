4166ae7ddf433ca58ae79755c3184d6fa7676f80
theOriginalMe <timi.parkinson@gmail.com>
2023-06-01 18:59:16 -0700

    Initial Boulder Commit

--- IceMan/IceMan/Actor.h
+++ IceMan/IceMan/Actor.h
@@ -6,67 +6,118 @@
 class StudentWorld;
 
 class Actor : public GraphObject {
-public:
+ public:
     Actor(int imageID, int startX, int startY, Direction dir = right, double size = 1.0, unsigned int depth = 0)
         : GraphObject(imageID, startX, startY, dir, size, depth) {}
     virtual ~Actor() {}
-    
+
     virtual void doSomething() = 0;
 };
 
 #pragma region Entities
 class Entity : public Actor {
-public:
+ public:
     Entity(int imageID, int startX, int startY): Actor(imageID, startX, startY) {}
     virtual ~Entity() {}
-    
+
     virtual void doSomething() = 0;
 };
 
 class Iceman : public Entity {
-private:
+ private:
     StudentWorld* m_studentWorldPointer;
-public:
-    Iceman(StudentWorld* sp = nullptr) : Entity(IID_PLAYER, 30, 60), m_studentWorldPointer(sp) { setVisible(true); }
+	int m_health;
+ public:
+    Iceman(StudentWorld* swp = nullptr) : Entity(IID_PLAYER, 30, 60), m_studentWorldPointer(swp) { setVisible(true); }
     virtual ~Iceman() {}
 
     StudentWorld* getWorld() const noexcept;
-    void doSomething();
+    virtual void doSomething() override;
+};
+
+class Protestor : public Entity {
+ private:
+	StudentWorld* m_studentWorldPointer;
+ public:
+	Protestor(StudentWorld* swp) : Entity(IID_PROTESTER, 60, 60), m_studentWorldPointer(swp) {setDirection(Direction::left); setVisible(true); }
+	virtual ~Protestor() {}
+
+	virtual void doSomething() override;
 };
 #pragma endregion Entities
 
-#pragma region GameObjects
+#pragma region Objects
 class Object : public Actor {
-public:
+ public:
     Object(int imageID, int startX = 0, int startY = 0, Direction dir = right, double size = 1.0, unsigned int depth = 0) : Actor(imageID, startX, startY, dir, size, depth) {}
     virtual ~Object(){}
-    
+
     virtual void doSomething() = 0;
 };
 
 class Terrain : public Object {
-public:
+ public:
     Terrain(int imageID, int startX, int startY, Direction dir = right, double size = 1.0, unsigned int depth = 0) : Object(imageID, startX, startY, dir, size, depth) {}
     Terrain(int imageID) : Object(imageID) {}
     virtual ~Terrain() {}
-    
+
     virtual void doSomething() = 0;
 };
 
 class Ice : public Terrain {
-public:
+ public:
     Ice(int startX, int startY, Direction dir = right, double size = .25, unsigned int depth = 3) : Terrain(IID_ICE, startX, startY, dir, size, depth) {
         setVisible(true);
-        //std::cout << "created ICE" << std::endl;
     }
     virtual ~Ice() {}
-    void doSomething() {}
+    virtual void doSomething() override {}
 };
-#pragma endregion GameObjects
 
+class Boulder : public Terrain {
+ private:
+	#pragma region State
+	class State {
+	 private:
+		Boulder* m_boulderPointer;
+	 public:
+	 	State() {}
+		State(Boulder* boulder) : m_boulderPointer(boulder) {}
+		virtual void doSomething() = 0;
+		virtual ~State();
+	};
+	class Idle : public State {
+	 private:
+		Boulder* m_boulderPointer;
+	 public:
+		Idle(Boulder* boulder) : m_boulderPointer(boulder) {}
+		virtual void doSomething() override;
+	};
+	class Waiting: public State {
+	 private:
+		Boulder* m_boulderPointer;
+	 public:
+		Waiting(Boulder* boulder) : m_boulderPointer(boulder) {}
+		virtual void doSomething() override;
+	};
+	class Falling : public State {
+	 private:
+		Boulder* m_boulderPointer;
+	 public:
+		Falling(Boulder* boulder) : m_boulderPointer(boulder) {}
+		virtual void doSomething() override;
+	};
+	#pragma endregion State
+	State* m_state;
+ public:
+	Boulder(int startX = 0, int startY = 0, Direction dir = right, double size = 1.0, unsigned int depth = 1) : Terrain(IID_BOULDER, startX, startY, dir, size, depth), m_state(new Idle(this)) {
+		setVisible(true);
+	}
+	virtual ~Boulder();
 
-
-
+	void setState(std::string s);
+	virtual void doSomething() override;
+};
+#pragma endregion Objects
 
 // Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
 
--- IceMan/IceMan/StudentWorld.h
+++ IceMan/IceMan/StudentWorld.h
@@ -7,35 +7,117 @@
 #include <array>
 #include <unordered_set>
 #include <string>
+#include <vector>
+#include <utility>
+#include <ranges>
+#include <random>
+#include <functional>
 
 class Actor;
 class Iceman;
 class Ice;
+class Boulder;
 
-// Students:  Add code to this file, StudentWorld.cpp, Actor.h, and Actor.cpp
+constexpr int MAX_SIZE = 64;
 
 class StudentWorld : public GameWorld {
  private:
+	#pragma region BlackList
+	template <typename T>
+	class BlackList {
+	 private:
+		std::array<std::pair<int, int>, MAX_SIZE> m_positions;
+	 public:
+		BlackList() {
+			init();
+		}
+		virtual ~BlackList() {}
+
+		void init () {
+			for (int x = 0; x < MAX_SIZE; x++) {
+				for (int y = 0; y < MAX_SIZE; y++){
+					if ((x >= 30 && x <= 33) && (y >= 4 && y <= MAX_SIZE - 1)) {
+						m_positions[m_positions.size() - 1] = std::make_pair(x, y);
+					}
+				}
+			}
+		}
+		void add(std::pair<int, int> p) {
+			if (!isListed(p)) {
+				m_positions[m_positions.size() - 1] = p;
+			}
+		}
+		bool isListed (std::pair<int, int> p) {
+			return std::ranges::find(m_positions, p) != m_positions.end();
+		}
+		bool isListed (int x, int y) {
+			return std::ranges::find(m_positions, std::make_pair(x, y)) != m_positions.end();
+		}
+	};
+
+	template <>
+	class BlackList<Boulder> : public BlackList<BlackList<Boulder>> {
+	 private:
+		std::array<std::pair<int, int>, MAX_SIZE> m_positions;
+		const int m_boulderPadding = 6;
+	 public:
+		BlackList() {
+			init();
+		}
+		virtual ~BlackList() {}
+		void add(std::pair<int, int> p) {
+			for (int i = 0; i < m_boulderPadding; i++) {
+				for (int j = 0; j < m_boulderPadding; j++) {
+					p = std::make_pair(p.first + i, p.second + j);
+					if (isListed(p.first, p.second)) {
+						continue;
+					} else {
+						m_positions[m_positions.size() - 1] = p;
+					}
+				}
+			}
+		}
+	};
+
+	#pragma endregion BlackList
+
+	#pragma region GameStats
 	class GameStats {
 	 private:
-		int m_level;
-		int m_lives;
-		int m_score;
-		int m_health;
-		int m_squirts;
-		int m_gold;
-		int m_barrels;
-		int m_sonar;
+	 	StudentWorld* m_studentWorldPointer;
 
-		int m_boulders;
+		int m_levelCount;
+		int m_lifeCount;
+		int m_scoreCount;
+		int m_healthCount;
+		int m_squirtCount;
+		int m_goldCount;
+		int m_barrelCount;
+		int m_sonarCount;
+		int m_boulderCount;
 	 public:
-		GameStats(StudentWorld* stw ) : m_level(stw->getLevel()), m_lives(stw->getLives()), m_score(stw->getScore()) {}
+		GameStats(StudentWorld* swp) : m_studentWorldPointer(swp), m_levelCount(swp->getLevel()), m_lifeCount(swp->getLives()), m_scoreCount(swp->getScore()) {}
         void init() noexcept;
         std::string toString() const noexcept;
+
+		// Getters
+		int getLevel() const noexcept;
+		int getLives() const noexcept;
+		int getScore() const noexcept;
+		int getHealth() const noexcept;
+		int getSquirts() const noexcept;
+		int getGold() const noexcept;
+		int getBarrels() const noexcept;
+		int getSonar() const noexcept;
+		int getBoulders() const noexcept;
 	};
+	#pragma endregion GameStats
+
+	#pragma region OilField
 	class OilField {
 	 private:
-	 	std::array<std::array<Ice*, 60>, 60> self;
+	 	std::array<std::array<Ice*, MAX_SIZE>, MAX_SIZE> self;
+		BlackList<Ice> m_iceBlackList;
 	 public:
 		OilField() {
 			for (auto& i : self) {
@@ -47,14 +129,22 @@ class StudentWorld : public GameWorld {
 
         void cleanUp() noexcept;
         void removeIce(int x, int y) noexcept;
+		bool isIce(int x, int y) const noexcept;
 		void init();
 	};
+	#pragma endregion OilField
+
+	#pragma region Stage
 	class Stage {
 	 private:
+		BlackList<Boulder> m_boulderBlackList;
 	 	std::unordered_set<Actor*> self;
-
+		StudentWorld* m_studentWorldPointer;
+		void spawnActor(Actor* actor) noexcept;
+		void spawnActor(Boulder* boulder) noexcept;
+        void removeActor(Actor* actor) noexcept;
 	 public:
-		Stage() {
+		Stage(StudentWorld* swp) : m_studentWorldPointer(swp) {
 			for (auto i : self) {
 				i = nullptr;
 			}
@@ -62,19 +152,18 @@ class StudentWorld : public GameWorld {
         ~Stage();
 
         void cleanUp() noexcept;
-        void addActor(Actor* actor) noexcept;
-        void removeActor(Actor* actor) noexcept;
         void init();
         void move();
 	};
-    
+    #pragma endregion Stage
+
 	GameStats m_stats;
     OilField m_oilField;
 	Stage m_stage;
     Iceman* m_iceman;
 
  public:
-	StudentWorld(std::string assetDir) : GameWorld(assetDir), m_iceman(nullptr), m_stats(this) {}
+	StudentWorld(std::string assetDir) : GameWorld(assetDir), m_iceman(nullptr), m_stats(this), m_stage(this) {}
     virtual ~StudentWorld();
 
 	virtual int init() override;
